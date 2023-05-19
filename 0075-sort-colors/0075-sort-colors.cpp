b551bd0ffbf7150237a44df6c86e3119ea2a1f89
D I V Y A <106904445+divyaaxyz@users.noreply.github.com>
2023-05-19 18:18:22 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (89.33%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,27 +3,45 @@ public:
     void sortColors(vector<int>& nums) {
         //time--- O(n)----- 2 passes
         //space--- O(1) ----3 var
-        int count0=0,count1=0,count2=0;
-        for(int i=0;i<size(nums);i++){
-            if(nums[i]==0){
-                count0++;
+        // int count0=0,count1=0,count2=0;
+        // for(int i=0;i<size(nums);i++){
+        //     if(nums[i]==0){
+        //         count0++;
+        //     }
+        //     else if(nums[i]==1){
+        //         count1++;
+        //     }
+        //     else{
+        //         count2++;
+        //     }
+        // }
+        // for(int i=0;i<size(nums);i++){
+        //     if(count0-->0){
+        //         nums[i]=0;
+        //     }
+        //     else if(count1-->0){
+        //         nums[i]=1;
+        //     }
+        //     else{
+        //         nums[i]=2;
+        //     }
+        // }
+        
+        
+        //single pass 
+        //dutch national flag algo = 3 way partitioning
+        int low=0,mid=0,high=size(nums)-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                mid++;low++;
             }
-            else if(nums[i]==1){
-                count1++;
+            else if(nums[mid]==1){
+                mid++;
             }
             else{
-                count2++;
-            }
-        }
-        for(int i=0;i<size(nums);i++){
-            if(count0-->0){
-                nums[i]=0;
-            }
-            else if(count1-->0){
-                nums[i]=1;
-            }
-            else{
-                nums[i]=2;
+                swap(nums[mid],nums[high]);
+                high--;
             }
         }
     }
