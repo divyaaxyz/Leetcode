00e8879d4b4513973d9b4bc84c916efa4d630882
D I V Y A <106904445+divyaaxyz@users.noreply.github.com>
2023-07-27 16:07:22 +0530

    Time: 16 ms (71.15%), Space: 13.1 MB (88.60%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,32 +1,97 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        //Brute- force
+        //TC- mn + mn(m+n)
+        //SC- 1
+        // int m=matrix.size();
+        // int n=matrix[0].size();
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(matrix[i][j]==0){
+        //             matrix[i][j]='a';
+        //         }
+        //     }
+        // }
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(matrix[i][j]=='a'){
+        //             matrix[i][j]=0;
+        //             int r=i;
+        //             for(int c=0;c<n;c++){
+        //                 if(matrix[r][c]!='a'){
+        //                    matrix[r][c]=0;
+        //                 }
+        //             }
+        //             int c=j;
+        //             for(int r=0;r<m;r++){
+        //                 if(matrix[r][c]!='a'){
+        //                    matrix[r][c]=0;
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        
+        //Better
+        //TC- 2mn
+        //SC- m+n
+        // int m=matrix.size();
+        // int n=matrix[0].size();
+        // int col[n]={0}; //m,n should be given 
+        // int row[m]={0};
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(matrix[i][j]==0){
+        //             row[i]=1;
+        //             col[j]=1;
+        //         }
+        //     }
+        // }
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(row[i]==1 || col[j]==1){
+        //             matrix[i][j]=0;
+        //         }
+        //     }
+        // }
+        
+        //Optimal
+        //TC- 2mn
+        //SC- 1
         int m=matrix.size();
         int n=matrix[0].size();
+        int col0=1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    matrix[i][j]='a';
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }
+                    else{
+                        col0=0;
+                    }
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]=='a'){
-                    matrix[i][j]=0;
-                    int r=i;
-                    for(int c=0;c<n;c++){
-                        if(matrix[r][c]!='a'){
-                           matrix[r][c]=0;
-                        }
-                    }
-                    int c=j;
-                    for(int r=0;r<m;r++){
-                        if(matrix[r][c]!='a'){
-                           matrix[r][c]=0;
-                        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0; //
                     }
                 }
+            }
+        }
+        if(matrix[0][0]==0){
+            for(int j=0;j<n;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(col0==0){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
             }
         }
     }
