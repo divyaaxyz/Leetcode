118fc4df9cdd3b2f05946672cbbdf2f9cf187eca
D I V Y A <106904445+divyaaxyz@users.noreply.github.com>
2023-08-15 16:55:53 +0530

    Time: 10 ms (30.96%), Space: 13.7 MB (61.82%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        //Naive----- TLE
+        //TC=O(n*n*n)
+        //SC=O(1)
+        // int n=nums.size(); //4
+        // int mxm=INT_MIN;
+        // for(int i=0;i<n;i++){ //0
+        //     for(int j=i;j<n;j++){ //0 1 2
+        //         int pdt=1;
+        //         for(int k=i;k<=j;k++){ //0 //0 1 //0 1 2
+        //             pdt*=nums[k]; //pdt=2 //6 //-12
+        //         }
+        //         mxm=max(mxm,pdt); //mxm=2 //6
+        //     }
+        // }
+        // return mxm;
+        
+        //Better----- TLE
+        //TC=O(n*n)
+        //SC=O(1)
+        // int n=nums.size();
+        // int mxm=INT_MIN;
+        // for(int i=0;i<n;i++){
+        //     int pdt=1;
+        //     for(int j=i;j<n;j++){
+        //         pdt*=nums[j];
+        //         mxm=max(mxm,pdt);
+        //     }
+        // }
+        // return mxm;
+        
+        //Optimal
+        //TC=O(n)
+        //SC=O(1)
+        int n=nums.size();
+        int pre=1,suf=1,mxm=INT_MIN;
+        for(int i=0;i<n;i++){
+            if(pre==0) pre=1;
+            if(suf==0) suf=1;
+            pre=pre*nums[i];
+            suf=suf*nums[n-i-1];
+            mxm=max(mxm,max(pre,suf));
+        }
+        return mxm;
+    }
+};
