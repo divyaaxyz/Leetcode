4d0d01508ef49142bf67a316bc5f543532d64522
D I V Y A <106904445+divyaaxyz@users.noreply.github.com>
2023-08-15 18:00:31 +0530

    Time: 7 ms (62.15%), Space: 13.7 MB (61.82%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -34,15 +34,26 @@ public:
         //Optimal
         //TC=O(n)
         //SC=O(1)
+        // int n=nums.size();
+        // int pre=1,suf=1,mxm=INT_MIN;
+        // for(int i=0;i<n;i++){
+        //     if(pre==0) pre=1;
+        //     if(suf==0) suf=1;
+        //     pre=pre*nums[i];
+        //     suf=suf*nums[n-i-1];
+        //     mxm=max(mxm,max(pre,suf));
+        // }
+        // return mxm;
+        
+        //Kadane's algo
+        int p1=nums[0],p2=nums[0],ans=nums[0];
         int n=nums.size();
-        int pre=1,suf=1,mxm=INT_MIN;
-        for(int i=0;i<n;i++){
-            if(pre==0) pre=1;
-            if(suf==0) suf=1;
-            pre=pre*nums[i];
-            suf=suf*nums[n-i-1];
-            mxm=max(mxm,max(pre,suf));
+        for(int i=1;i<n;i++){
+            int tmp=max({nums[i],p1*nums[i],p2*nums[i]});
+            p2=min({nums[i],p1*nums[i],p2*nums[i]});
+            p1=tmp;
+            ans=max(ans,p1);
         }
-        return mxm;
+        return ans;
     }
 };
