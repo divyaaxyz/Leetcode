875e9ff3d1eec33ff5b103ad41ea4df4ba3a74b8
D I V Y A <106904445+divyaaxyz@users.noreply.github.com>
2023-08-16 16:29:17 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (66.59%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        //Brute force
+        //TC=O(m*n)
+        //SC=O(1)
+        // int m=matrix.size(),n=matrix[0].size();
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(matrix[i][j]==target){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;   
+        
+        //Optimal
+        //TC=O(log (m*n))
+        //SC=O(1)
         int m=matrix.size(),n=matrix[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-            }
+        int low=0,high=(m*n)-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int row=mid/n;
+            int col=mid%n;
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]<target) low=mid+1;
+            else high=mid-1;
         }
-        return false;      
+        return false;
     }
 };
