443227b70f4baef7d005146070339e6ac3edaa1d
D I V Y A <106904445+divyaaxyz@users.noreply.github.com>
2023-08-24 10:40:36 +0530

    Time: 13 ms (66.15%), Space: 11.2 MB (16.08%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int>idx;
+        //Brute force
+        //TC=O(n*n)
+        //SC=O(1)
+        // vector<int>idx;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[i]+nums[j]==target){
+        //             idx.push_back(i);
+        //             idx.push_back(j);
+        //             return idx;
+        //         }
+        //     }
+        // }
+        // return {-1,-1};
+        
+        //Optimal
+        //SC=O(n)
+        //TC=O(n * logn)
+        map<int,int>mpp; //map data structure - element, index store
         int n=nums.size();
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]==target){
-                    idx.push_back(i);
-                    idx.push_back(j);
-                    return idx;
-                }
+            int first=nums[i]; //first element
+            int sec=target-first; //what second element should be
+            if(mpp.find(sec)!=mpp.end()){ //finding second element in map till end- if point to end then didn't find
+                return {mpp[sec],i}; //second index return= mpp[sec], first index
             }
+            mpp[first]=i;//if not in map, put the first element we took- in map
         }
         return {-1,-1};
     }
